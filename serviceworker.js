a4dff9b3ecf843caaeecfe473bf1748a033e09a4
andreasdelu <Andreasdelu@gmail.com>
2022-10-04 13:00:59 +0200

    WOOOOORKS

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,41 @@
-const cacheName = "cahce-insects";
+const cacheName = "cache-insects";
+
+const cacheItems = [
+	"./",
+	"./manifest.json",
+	"./serviceworker.js",
+	"./index.html",
+	"./butterflies.jpg",
+	"./butterfly.jpg",
+	"./dragonfly.jpg",
+	"./menu.svg",
+	"./long.png",
+	"./icon-512.png",
+	"./icon-192.png",
+];
 
 self.addEventListener("install", function (e) {
 	e.waitUntil(
-		caches.open(cacheName).then(function (cache) {
-			return cache.addAll([
-				"/pwa/",
-				"/pwa/index.html",
-				"/pwa/butterflies.jpg",
-				"/pwa/butterfly.jpg",
-				"/pwa/dragonfly.jpg",
-				"/pwa/menu.svg",
-			]);
-		})
+		(async () => {
+			const cache = await caches.open(cacheName);
+			const toCache = await cache.addAll(cacheItems);
+			return toCache;
+		})()
 	);
 });
 
-self.addEventListener("fetch", function (e) {
+self.addEventListener("fetch", async function (e) {
 	e.respondWith(
-		fetch(e.request).catch(() => {
-			caches.open(cacheName).then((cache) => cache.match(e.request));
-		})
+		(async () => {
+			// Try to get the response from a cache.
+			const cachedResponse = await caches.match(e.request, {
+				ignoreVary: true,
+			});
+			// Return it if we found one.
+			if (cachedResponse) return cachedResponse;
+			// If we didn't find a match in the cache, use the network.
+			const response = fetch(e.request);
+			return response;
+		})()
 	);
 });
